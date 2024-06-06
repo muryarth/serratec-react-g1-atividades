68d3d921e1e5d72df4b9c01446a71554b75ba306
muryarth <arthur.mury@outlook.com>
2024-06-06 17:28:08 -0300

    feat: subindo navbar redirecionando para rotas

--- trabalho01/src/App.jsx
+++ trabalho01/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <AppRouter />
     </BrowserRouter>
   );
--- trabalho01/src/components/Header/index.jsx
+++ trabalho01/src/components/Header/index.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <ul>
-      <li>
-        <Link to={"/"}>Exercício 1</Link>
-      </li>
-      <li>
-        <Link to={"/exercicio2"}>Exercício 2</Link>
-      </li>
-      <li>
-        <Link to={"/exercicio3"}>Exercício 3</Link>
-      </li>
-      <li>
-        <Link to={"/exercicio4"}>Exercício 4</Link>
-      </li>
-    </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link" to={"/"}>
+            Exercício 1
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to={"/exercicio2"}>
+            Exercício 2
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to={"/exercicio3"}>
+            Exercício 3
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to={"/exercicio4"}>
+            Exercício 4
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
